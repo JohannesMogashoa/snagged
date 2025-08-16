02ca8e9f16480a014d940e171fc9e21dbe46d0cd
Johannes Mogashoa <jomogashoa1993@gmail.com>
2025-08-16 19:39:26 +0200

    basic basic styling

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,10 +2,8 @@ import { SigninForm } from "@/app/_components/auth/sign-in-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SigninForm />
-      </div>
+    <div className="w-full max-w-sm">
+      <SigninForm />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import SignoutButton from "./_components/auth/sign-out-button";
 import { getServerSession } from "@/server/auth";
 
 export default async function Home() {
-  const session = await getServerSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-
-            {!session ? (
-              <Link
-                href={session ? "/signout" : "/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            ) : (
-              <SignoutButton />
-            )}
-          </div>
-        </div>
-      </main>
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        Welcome to <span className="text-[#15162c]">Snagged</span>
+      </h1>
+      <Link href={"/signin"} className="cursor-pointer">
+        <Button variant={"outline"}>Get Started</Button>
+      </Link>
+    </div>
   );
 }
